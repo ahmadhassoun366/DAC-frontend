434b4ef4bded0f3570a460598dbba56a0c58a446
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-08-17 23:14:27 -0700

    updating colors

--- frontend/src/layout/header-layout/header.jsx
+++ frontend/src/layout/header-layout/header.jsx
@@ -1,44 +1,99 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const header = () => {
-    return (
+  return (
+    <header className="bg-CustomColor1">
+      <nav className="container mx-auto p-6 lg:flex lg:items-center lg:justify-between">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link
+              className="text-2xl font-bold text-gray-200  lg:text-3xl"
+              href="#"
+            >
+              DAC
+            </Link>
+          </div>
 
-        <header className="bg-CustomColor1" >
-            <nav className="container mx-auto p-6 lg:flex lg:items-center lg:justify-between">
-                <div className="flex items-center justify-between">
-                    <div>
-                        <a className="text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl" href="#">DAC</a>
-                    </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="text-gray-200 hover:text-gray-200 focus:text-blue-600 focus:outline-none "
+              aria-label="toggle menu"
+            >
+              <svg
+                x-show="!isOpen"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4 8h16M4 16h16"
+                />
+              </svg>
 
+              <svg
+                x-show="isOpen"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
 
-                    <div className="flex lg:hidden">
-                        <button type="button" className="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400" aria-label="toggle menu">
-                            <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-                            </svg>
+        <div className="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none">
+          <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
+            <Link
+              className="text-gray-200 hover:text-blue-500  lg:mx-6"
+              href="#"
+            >
+              Home
+            </Link>
+            <Link
+              className="text-gray-200 hover:text-blue-500 lg:mx-6"
+              href="#"
+            >
+              Pricing
+            </Link>
+            <Link
+              className="text-gray-200 hover:text-blue-500 lg:mx-6"
+              href="#"
+            >
+              Contact
+            </Link>
+            <Link
+              className="text-gray-200 hover:text-blue-500 lg:mx-6"
+              href="#"
+            >
+              FAQ
+            </Link>
+          </div>
 
-                            <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-
-
-                <div className="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-gray-900 lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent">
-                    <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
-                        <a className="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6" href="#">Home</a>
-                        <a className="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6" href="#">Pricing</a>
-                        <a className="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6" href="#">Contact</a>
-                        <a className="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6" href="#">FAQ</a>
-                    </div>
-
-                    <a className="mt-4 block h-10 transform rounded-md border px-5 py-2 text-center text-sm capitalize text-gray-700 transition-colors duration-300 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 lg:mt-0 lg:w-auto mr-4" href="#">Sign In</a>
-                    <a className="mt-4 block h-10 transform rounded-md border px-5 py-2 text-center text-sm capitalize text-gray-700 transition-colors duration-300 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 lg:mt-0 lg:w-auto" href="#">Sign Up</a>
-                </div>
-            </nav>
-        </header>
-    );
+          <Link
+            className="mt-4 block h-10 transform rounded-md border px-5 py-2 text-center text-sm capitalize text-gray-200 transition-colors duration-300 hover:bg-gray-100  hover:text-gray-700 lg:mt-0 lg:w-auto mr-4"
+            href="#"
+          >
+            Sign In
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default header;
