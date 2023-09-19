fec1aeb3c8f9b1a28636214326961efc8636b9ac
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-09-18 23:17:21 -0700

    refactor code

--- frontend/src/pages/Dashboard/MainContent/MainContent.jsx
+++ frontend/src/pages/Dashboard/MainContent/MainContent.jsx
@@ -7,60 +7,47 @@ import Items from "./Items/Items";
 import ItemsDetails from "./Items/ItemsDetails";
 
 const MainContent = () => {
-    const [currentitem, setcurrentitem] = useState({ supcode: 'a1111', });
-    const [itemID, setitemID] = useState(null);
+  const [currentitem, setcurrentitem] = useState({ supcode: "a1111" });
+  const [itemID, setitemID] = useState(null);
 
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const onEdit = () => {
-        // alert("edit");
-    }
-    const onDelete = (currentitem) => {
-        alert("onDelete");
-        fetch(`http://127.0.0.1:8000/api/item/delete/${currentitem.id}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                alert("done");
-                setLoading(false);
-            })
-            .catch((error) => {
-                setError(error);
-                setLoading(true);
-            });
-    }
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const onEdit = () => {
+    // alert("edit");
+  };
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-    console.log("sssssssscurrentitem", currentitem);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <main>
-            <Routes>
-                <Route index element={<MainDashboard />} />
-                <Route path="stock" element={<Stock />} />
-                <Route path="Stock/Items" element={<Items setcurrentitem={setcurrentitem} currentitem={currentitem} />} />
-                <Route path="Stock/Items/addItems" element={<Additem />} />
-                <Route path="Stock/items/itemdetails"
-                    element={
-                        <ItemsDetails
-                            item={currentitem}
-                            onEdit={onEdit}
-                            onDelete={onDelete}
-                        />}
-                />
-            </Routes>
-        </main>
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
-    )
+  return (
+    <main>
+      <Routes>
+        <Route index element={<MainDashboard />} />
+        <Route path="stock" element={<Stock />} />
+        <Route
+          path="Stock/Items"
+          element={
+            <Items setcurrentitem={setcurrentitem} currentitem={currentitem} />
+          }
+        />
+        <Route path="Stock/Items/addItems" element={<Additem />} />
+        <Route
+          path="Stock/items/itemdetails"
+          element={
+            <ItemsDetails
+              item={currentitem}
+              onEdit={onEdit}
+            />
+          }
+        />
+      </Routes>
+    </main>
+  );
 };
 
 export default MainContent;
