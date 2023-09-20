14a3523eac0e0d6b55461471b442e1829894bed6
younes alkhatib <81439514+YounesAlkhatibCX@users.noreply.github.com>
2023-09-20 16:32:59 +0300

    update

--- frontend/src/pages/Dashboard/MainContent/Accounting/Accounting.jsx
+++ frontend/src/pages/Dashboard/MainContent/Accounting/Accounting.jsx
@@ -5,7 +5,20 @@ import van from "../../../../Assets/van.png";
 
 const Accounting = () => {
     return (
-        <div class="pt-6 px-4"><h1>Accounting</h1></div>
+        <div class="pt-6 px-4">
+            <div class="flex justify-around p-10 gap-8">
+                <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg">
+                    <div class="flex justify-center md:justify-end -mt-16">
+                        <img class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src={van} alt="" />
+                    </div>
+                    <div>
+                        <h2 class="text-gray-800 text-3xl font-semibold">Units</h2>
+                        <p class="mt-2 text-gray-600">Manage your company Unit, Add units ,Delete and edits  </p>
+                        <Link to="./units" class="mt-4 bg-cyan-600 text-white px-6 py-2 rounded inline-flex items-center">Visit</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Accounting;
