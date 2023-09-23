543cbcf9cd21e3e739b8eb33cd91e62a9e59bf62
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-09-23 03:40:29 -0700

    up

--- frontend/src/pages/Dashboard/MainContent/Accounting/Units/Editunit.jsx
+++ frontend/src/pages/Dashboard/MainContent/Accounting/Units/Editunit.jsx
@@ -1,93 +1,182 @@
 import React, { useState, useEffect } from "react";
 
-const EditUnit = ({ unitDetails, closeModal }) => {
-    const [unitName, setUnitName] = useState("");
-    const [symbol, setSymbol] = useState("");
-    const [subUnit, setSubUnit] = useState("");
-    const [operation, setOperation] = useState("");
-    const [amount, setAmount] = useState("");
-    const [isEditing, setIsEditing] = useState(false);
+const EditUnit = (props) => {
+  const [unitName, setUnitName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [subUnit, setSubUnit] = useState("");
+  const [operation, setOperation] = useState("");
+  const [amount, setAmount] = useState("");
 
-    // useEffect(() => {
-    //     // If unitDetails prop changes, update the local state
-    //     setUnitName(unitDetails.name);
-    //     setSymbol(unitDetails.symbol);
-    //     setSubUnit(unitDetails.subUnit);
-    //     setOperation(unitDetails.operation);
-    //     setAmount(unitDetails.amount);
-    // }, [unitDetails]);
+  useEffect(() => {
+    // When component mounts, populate state with props data
+    if (props.unit) {
+      setUnitName(props.unit.name);
+      setSymbol(props.unit.unit_symbol);
+      setSubUnit(props.unit.sub_unit);
+      setOperation(props.unit.operation);
+      setAmount(props.unit.amount);
+    }
+  }, [props.unit]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const payload = {
-            name: unitName,
-            unit_symbol: symbol,
-            sub_unit: subUnit,
-            operation,
-            amount,
-        };
-
-        try {
-            const response = await fetch(`http://127.0.0.1:8000/api/unit/${unitDetails.id}/edit`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(payload)
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                console.log("Successfully updated:", data);
-                // Here you can navigate the user to another page or show a success message
-            } else {
-                console.error("Error:", response.statusText);
-            }
-        } catch (error) {
-            console.error("There was an error updating the unit", error);
-        }
+    const payload = {
+      name: unitName,
+      unit_symbol: symbol,
+      sub_unit: subUnit,
+      operation,
+      amount,
     };
 
-    return  (
-        <div className="py-6 px-4">
-            <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-2xl font-semibold mb-4">Edit Unit</h2>
-                
-                {!isEditing ? (
-                    <div>
-                        <p><strong>Unit Name:</strong> {unitName}</p>
-                        <p><strong>Symbol:</strong> {symbol}</p>
-                        <p><strong>Sub Unit:</strong> {subUnit}</p>
-                        <p><strong>Operation:</strong> {operation}</p>
-                        <p><strong>Amount:</strong> {amount}</p>
-    
-                        <button onClick={() => setIsEditing(true)} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg">
-                            Edit
-                        </button>
-                    </div>
-                ) : (
-                    <form onSubmit={handleSubmit}>
-                        {/* ... Your existing form fields ... */}
-                        <button
-                            type="submit"
-                            className="w-full bg-black text-white py-2 px-4 rounded-lg hover:bg-cyan-700 transition duration-300"
-                        >
-                            Save Changes
-                        </button>
-                    </form>
-                )}
-    
-                <div className="mt-4 text-center">
-                    <button
-                        onClick={closeModal}
-                        className="text-black-500 hover:underline">
-                        Cancel
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-};
+    try {
+      // Adjust the API endpoint and method for editing
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/unit/edit/${props.unit.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
 
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Edited:", data);
+        // Additional success actions (e.g., updating UI, navigating, etc.)
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("There was an error editing the unit", error);
+    }
+  };
+
+  return (
+    <div className="py-6 px-4">
+      <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Edit Unit</h2>
+        <form onSubmit={handleSubmit}>
+          {/* Unit Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="unitName"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Unit Name
+            </label>
+            <input
+              type="text"
+              id="unitName"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-400"
+              placeholder="Enter unit name"
+              value={unitName}
+              onChange={(e) => setUnitName(e.target.value)}
+              required
+            />
+          </div>
+          {/* Amount */}
+          <div className="mb-4">
+            <label
+              htmlFor="amount"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Amount
+            </label>
+            <input
+              type="number"
+              id="amount"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-400"
+              placeholder="Enter amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              required
+            />
+          </div>
+          {/* Operation */}
+          <div className="mb-4">
+            <label
+              htmlFor="operation"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Operation
+            </label>
+            <select
+              id="operation"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-400"
+              value={operation}
+              onChange={(e) => setOperation(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                Select operation
+              </option>
+              <option value="+">Addition (+)</option>
+              <option value="-">Subtraction (-)</option>
+              <option value="*">Multiplication (*)</option>
+              <option value="/">Division (/)</option>
+            </select>
+          </div>
+          {/* Symbol */}
+          <div className="mb-4">
+            <label
+              htmlFor="symbol"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Symbol
+            </label>
+            <input
+              type="text"
+              id="symbol"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-400"
+              placeholder="Enter symbol"
+              value={symbol}
+              onChange={(e) => setSymbol(e.target.value)}
+              required
+            />
+          </div>
+          {/* Sub Unit */}
+          <div className="mb-4">
+            <label
+              htmlFor="subUnit"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Sub Unit
+            </label>
+            <select
+              id="subUnit"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-400"
+              value={subUnit}
+              onChange={(e) => setSubUnit(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                Select sub unit
+              </option>
+              <option value="1">Sub Unit 1</option>
+              <option value="2">Sub Unit 2</option>
+            </select>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 px-4 rounded-lg hover:bg-cyan-700 transition duration-300"
+          >
+            Save Changes
+          </button>
+        </form>
+
+        <div className="mt-4 text-center">
+          <button
+            className="text-black-500 hover:underline"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default EditUnit;
--- frontend/src/pages/Dashboard/MainContent/Accounting/Units/Units.jsx
+++ frontend/src/pages/Dashboard/MainContent/Accounting/Units/Units.jsx
@@ -13,26 +13,26 @@ const Units = () => {
   const [editingUnit, setEditingUnit] = useState(null);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  
+
   const history = useNavigate();
 
   const openAddModal = () => {
     setIsAddModalOpen(true);
-};
+  };
 
-const closeAddModal = () => {
+  const closeAddModal = () => {
     setIsAddModalOpen(false);
-};
+  };
 
-const openEditModal = (unit) => {
+  const openEditModal = (unit) => {
     setEditingUnit(unit);
     setIsEditModalOpen(true);
-};
+  };
 
-const closeEditModal = () => {
+  const closeEditModal = () => {
     setIsEditModalOpen(false);
     setEditingUnit(null); // optional, in case you want to clear out the editing unit
-};
+  };
   useEffect(() => {
     fetchUnits();
   }, []);
@@ -68,7 +68,7 @@ const closeEditModal = () => {
     <div className="pt-6 px-4">
       <div className="flex justify-end items-center mr-10">
         <button
-         onClick={openAddModal}
+          onClick={openAddModal}
           className="mt-4 bg-gray-900 text-white px-2 py-2 rounded inline-flex items-center"
         >
           + Add Unit
@@ -78,9 +78,12 @@ const closeEditModal = () => {
         <table className="w-full bg-white border rounded-lg shadow-lg">
           <thead>
             <tr>
-              <th className="border px-4 py-2">unitName</th>
-              <th className="border px-4 py-2">symbol</th>
+              <th className="border px-4 py-2">Unit Name</th>
+              <th className="border px-4 py-2">Symbol</th>
               {/* ... other headers */}
+              <th className="border px-4 py-2">Sub Unit</th>
+              <th className="border px-4 py-2">Operation</th>
+              <th className="border px-4 py-2">Amount</th>
               <th className="border px-4 py-2">Actions</th>
             </tr>
           </thead>
@@ -91,6 +94,8 @@ const closeEditModal = () => {
                 <td className="border px-4 py-2">{unit.name}</td>
                 <td className="border px-4 py-2">{unit.unit_symbol}</td>
                 <td className="border px-4 py-2">{unit.sub_unit}</td>
+                <td className="border px-4 py-2">{unit.operation}</td>
+                <td className="border px-4 py-2">{unit.amount}</td>
                 {/* ... other data cells */}
                 <td className="border px-4 py-2 flex justify-center items-center">
                   <button
@@ -100,7 +105,7 @@ const closeEditModal = () => {
                     Delete
                   </button>
                   <button
-                 onClick={() => openEditModal(unit)}
+                    onClick={() => openEditModal(unit)}
                     className="bg-gray-900 text-white px-5 py-1 rounded mr-1"
                   >
                     Edit
@@ -111,13 +116,19 @@ const closeEditModal = () => {
           </tbody>
         </table>
       </div>
+
       <div
-        className={`fixed inset-0 z-50 ${isEditModalOpen  ? "" : "hidden"}`}
+        className={`fixed inset-0 z-50 ${isAddModalOpen ? "" : "hidden"}`}
         style={{ top: "auto", left: "60%" }}
       >
         <Addunit closeModal={closeAddModal} />
-        <Editunit  unitDetails={editingUnit} closeModal={closeEditModal} />
+      </div>
 
+      <div
+        className={`fixed inset-0 z-50 ${isEditModalOpen ? "" : "hidden"}`}
+        style={{ top: "auto", left: "60%" }}
+      >
+        <Editunit unitDetails={editingUnit} loseModal={closeEditModal} />
       </div>
     </div>
   );
